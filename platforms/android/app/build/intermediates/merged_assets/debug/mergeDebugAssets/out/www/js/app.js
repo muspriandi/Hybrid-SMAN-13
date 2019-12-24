c5bb5891994563bb5559531cc4edfca7e30952a7
muspriandi <1.muspriandi@gmail.com>
2019-12-24 16:14:37 +0700

    Add menu masuk(login) dengan database MySQL

--- platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/js/app.js
+++ platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['starter.controllers','ionic'])
+angular.module('starter', ['starter.controllers','ionic','starter.siswa','starter.matapelajaran','starter.rapor'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -34,11 +34,6 @@ angular.module('starter', ['starter.controllers','ionic'])
     url: '/front',
     templateUrl: 'templates/front.html'
   })
-  .state('struktur', {
-    url: '/struktur',
-    templateUrl: 'templates/struktur.html',
-    controller: 'GeneralCtrl'
-  })
   .state('profil', {
     url: '/profil',
     templateUrl: 'templates/profil.html',
@@ -68,7 +63,180 @@ angular.module('starter', ['starter.controllers','ionic'])
     url: '/location',
     templateUrl: 'templates/location.html',
     controller: 'MapCtrl'
-  });
+  })
+  .state('masuk', {
+    url: '/masuk',
+    cache: false,
+    templateUrl: 'templates/masuk.html',
+    controller: 'LoginCtrl'
+  })
+
+  //  FUNGSI UNTUK ADMIN
+  .state('admin', {
+    url: '/admin',
+    abstract: true,
+    cache: false,
+    templateUrl: function() {
+      if(sessionStorage.getItem('login_status') == "admin") {
+        return 'templates/admin/menu.html'
+      }
+      else {
+        return 'templates/front.html'
+      }
+    }
+  })
+  // DASHBOARD
+  .state('admin.home', {
+    url: '/home',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/home.html',
+        controller: 'AdminCtrl'
+      }
+    }
+  })
+  // CRUD SISWA
+  .state('admin.r_siswa', {
+    url: '/r_siswa',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/r_siswa.html',
+        controller: 'DataSiswaCtrl'
+      }
+    }
+  })
+  .state('admin.c_siswa', {
+    url: '/c_siswa',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/c_siswa.html',
+        controller: 'siswaCtrl'
+      }
+    }
+  })
+  .state('admin.u_siswa', {
+    url: '/u_siswa/:nis',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/u_siswa.html',
+        controller: 'siswaCtrl'
+      }
+    }
+  })
+  // CRUD MATA PELAJARAN
+  .state('admin.r_mapel', {
+    url: '/r_mapel',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/r_mapel.html',
+        controller: 'DataMapelCtrl'
+      }
+    }
+  })
+  .state('admin.c_mapel', {
+    url: '/c_mapel',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/c_mapel.html',
+        controller: 'mapelCtrl'
+      }
+    }
+  })
+  .state('admin.u_mapel', {
+    url: '/u_mapel/:kdmapel',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/u_mapel.html',
+        controller: 'mapelCtrl'
+      }
+    }
+  })
+  // CRUD RAPOR
+  .state('admin.r_rapor', {
+    url: '/r_rapor',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/r_rapor.html',
+        controller: 'DataRaporCtrl'
+      }
+    }
+  })
+  .state('admin.c_rapor', {
+    url: '/c_rapor',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/c_rapor.html',
+        controller: 'raporCtrl'
+      }
+    }
+  })
+  .state('admin.u_rapor', {
+    url: '/u_rapor/:id',
+    cache: false,
+    views: {
+      'menuContentAdmin' : {
+        templateUrl: 'templates/admin/u_rapor.html',
+        controller: 'raporCtrl'
+      }
+    }
+  })
+
+  // FUNGSI UNTUK SISWA
+  .state('siswa', {
+    url: '/siswa',
+    abstract: true,
+    cache: false,
+    templateUrl: function() {
+      if(sessionStorage.getItem('login_status') == "siswa") {
+        return 'templates/siswa/menu.html'
+      }
+      else {
+        return 'templates/front.html'
+      }
+    }
+  })
+  // DASHBOARD
+  .state('siswa.home', {
+    url: '/home',
+    cache: false,
+    views: {
+      'menuContentSiswa' : {
+        templateUrl: 'templates/siswa/home.html',
+        controller: 'SiswaCtrl'
+      }
+    }
+  })
+  // UBAH KATA SANDI
+  .state('siswa.u_passSiswa', {
+    url: '/u_passSiswa',
+    cache: false,
+    views: {
+      'menuContentSiswa' : {
+        templateUrl: 'templates/siswa/u_passSiswa.html',
+        controller: 'ubahPassCtrl'
+      }
+    }
+  })
+  // VIEW RAPOR
+  .state('siswa.raporSiswa', {
+    url: '/raporSiswa',
+    cache: false,
+    views: {
+      'menuContentSiswa' : {
+        templateUrl: 'templates/siswa/raporSiswa.html',
+        controller: 'raporSiswaCtrl'
+      }
+    }
+  })
 
   $urlRouterProvider.otherwise('/front');
 })
--- platforms/android/app/src/main/assets/www/js/controllers.js
+++ platforms/android/app/src/main/assets/www/js/controllers.js
@@ -28,7 +28,7 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('MapCtrl', function($scope, $ionicLoading , $compile, $state, $window) {
+.controller('MapCtrl', function($scope, $ionicLoading, $compile, $state, $window) {
     window.initMap = function ()  {
         var myLatlng = new google.maps.LatLng(-6.119204, 106.895176);
      
@@ -62,4 +62,119 @@ angular.module('starter.controllers', [])
     $scope.doBack = function() {
         $window.history.back();
     };
+})
+
+.controller('LoginCtrl', function($scope, $http, $state, $ionicHistory, $window) {
+    var url = "http://localhost/API-SMAN13/";
+
+    $scope.loginData = {};
+
+    $scope.doLogin = function() {
+        var adm_user    = $scope.loginData.userid;
+        var adm_pass    = $scope.loginData.userpass;
+
+        if(adm_user == undefined || adm_user== undefined) {
+            swal ( "Oops..." ,  "User ID dan Kata Sandi harus diisi." ,  "error" );
+        }
+        else {
+            if(adm_user && adm_pass) {
+                str = url+"login.php?id="+adm_user+"&pass="+adm_pass;
+
+                $http.get(str)
+                .success(function(response) {
+                    if(response!='') {
+                        $scope.u = response.records;
+                        
+                        sessionStorage.setItem('login_status', $scope.u.status_user);
+                        sessionStorage.setItem('login_nis', $scope.u.nis_user);
+                        sessionStorage.setItem('login_name', $scope.u.nama_user);
+                        sessionStorage.setItem('login_alamat', $scope.u.alamat_user);
+                        sessionStorage.setItem('login_jen_kel', $scope.u.jen_kel_user);
+
+                        $ionicHistory.nextViewOptions ({
+                            disableAnime: true,
+                            disableBack: true
+                        })
+                        
+                        // JIKA USER == ADMIN SISTEM
+                        if($scope.u.status_user == "admin") {
+                            swal ( "Berhasil" ,  "Selamat datang Administrator." ,  "success" , {
+                                buttons: false,
+                                closeOnClickOutside: false,
+                                timer: 2000,
+                            })
+                            $state.go('admin.home', {id: $scope.u.nis}, {location:'replace', reload:true});
+                        }
+                        // JIKA USER == SISWA BIASA
+                        else {
+                            swal ( "Berhasil" ,  "Selamat datang "+$scope.u.nama_user+" ["+$scope.u.nis_user+"]."  ,  "success" , {
+                                buttons: false,
+                                closeOnClickOutside: false,
+                                timer: 2000,
+                            })
+                            $state.go('siswa.home', {id: $scope.u.nis}, {location:'replace', reload:true});
+                        }
+                    }
+                    else {
+                        swal ( "Oops..." ,  "User ID atau Kata Sandi salah." ,  "error" );
+                    }
+                })
+            }
+            else {
+                swal ( "Oops..." ,  "User ID atau Kata Sandi salah." ,  "error" );
+            }
+        }
+    }
+
+    $scope.doLogout = function() {
+        sessionStorage.removeItem('login_status');
+        sessionStorage.removeItem('login_nis');
+        sessionStorage.removeItem('login_id');
+        sessionStorage.removeItem('login_name');
+        sessionStorage.removeItem('login_alamat');
+        sessionStorage.removeItem('login_jen_kel');
+
+        $ionicHistory.nextViewOptions({
+            disableAnime: true,
+            disableBack: true
+        })
+
+        swal ( "Berhasil" ,  "Anda berhasil keluar dari Sistem." ,  "info", {
+            buttons: false,
+            closeOnClickOutside: false,
+            timer: 2000,
+        })
+
+        $state.go('front', {}, {location:'replace', reload:true});
+    }
+
+    $scope.doBack = function() {
+        $window.history.back();
+    };
+})
+
+//ADMIN
+.controller('AdminCtrl',function($scope, $http) {
+    var url = "http://localhost/API-SMAN13/";
+
+    $scope.nis  = sessionStorage.getItem('login_nis');
+    $scope.nama = sessionStorage.getItem('login_name');
+
+    $http.get(url+"siswa/totalSiswa.php").success(function(response) {
+        $scope.totalSiswa = response.records;
+    });
+
+    $http.get(url+"mapel/totalMapel.php").success(function(response) {
+        $scope.totalMapel = response.records;
+    });
+})
+
+//SISWA
+.controller('SiswaCtrl',function($scope, $http) {
+    var url = "http://localhost/API-SMAN13/";
+
+    $scope.nis      = sessionStorage.getItem('login_nis');
+    $scope.nama     = sessionStorage.getItem('login_name');
+    $scope.jen_kel  = sessionStorage.getItem('login_jen_kel');
+    $scope.alamat   = sessionStorage.getItem('login_alamat');
 })
